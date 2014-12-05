2adebd5f8f573ddbb1ee06084256d5b80c0eb327
Maciej <maciej@trebacz.org>
2014-12-05 12:29:32 +0100

    Wyświetlanie postów z bazy danych; dodanie paczki do parsowania czasu

--- linijka.js
+++ linijka.js
@@ -1,32 +1,15 @@
+Posts = new Meteor.Collection("posts");
+
 if (Meteor.isClient) {
   Template.postList.helpers({
       posts: function () {
-          return [
-            {
-                title: "Afera zbożowa - znamy dalsze szczegóły",
-                link: "http://wykop.pl",
-                time: "08:12",
-                author: "elfik_wie",
-                comments_num: 88,
-                votes: 102
-            },
-            {
-                title: "Gruntowne zmiany w serwisie Wykop.pl. Od teraz to użytkownicy decydują o tym, co pojawi się na głównej. Rzecznik wypowiedział się...",
-                link: "http://onet.pl",
-                time: "04:24",
-                author: "fojerfest",
-                comments_num: 2,
-                votes: 8
-            },
-            {
-                title: "Prośba o Linijka-Efekt! Chytra baba znowu atakuje",
-                link: "http://linijka.meteor.com",
-                time: "12:42",
-                author: "maver",
-                comments_num: 42,
-                votes: 47
-            },
-          ]
+          return Posts.find().fetch();
+      }
+  });
+
+  Template.postListItem.helpers({
+      time: function() {
+          return moment(this.added_on).fromNow();
       }
   });
 }
