357b852123a04d89063e7d70ffbaf7a2ae2457b5
Maciej <maciej@trebacz.org>
2014-12-05 11:53:02 +0100

    Podzielenie HTML na szablony; ładowanie danych z kodu Javascript

--- linijka.js
+++ linijka.js
@@ -1,23 +1,32 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.postList.helpers({
+      posts: function () {
+          return [
+            {
+                title: "Afera zbożowa - znamy dalsze szczegóły",
+                link: "http://wykop.pl",
+                time: "08:12",
+                author: "elfik_wie",
+                comments_num: 88,
+                votes: 102
+            },
+            {
+                title: "Gruntowne zmiany w serwisie Wykop.pl. Od teraz to użytkownicy decydują o tym, co pojawi się na głównej. Rzecznik wypowiedział się...",
+                link: "http://onet.pl",
+                time: "04:24",
+                author: "fojerfest",
+                comments_num: 2,
+                votes: 8
+            },
+            {
+                title: "Prośba o Linijka-Efekt! Chytra baba znowu atakuje",
+                link: "http://linijka.meteor.com",
+                time: "12:42",
+                author: "maver",
+                comments_num: 42,
+                votes: 47
+            },
+          ]
+      }
   });
 }
