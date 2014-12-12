fad960636b4b37e40baa2d4bb86d50444c5cb21a
Maciej <maciej@trebacz.org>
2014-12-12 19:13:08 +0100

    Dodawanie nowego postu

--- linijka.js
+++ linijka.js
@@ -7,7 +7,7 @@ if (Meteor.isClient) {
   
   Template.postList.helpers({
       posts: function () {
-          return Posts.find().fetch();
+          return Posts.find({}, {sort: {added_on: -1}}).fetch();
       },
 
   });
@@ -15,6 +15,9 @@ if (Meteor.isClient) {
   Template.postListItem.helpers({
       time: function() {
           return moment(this.added_on).fromNow();
+      },
+      author: function() {
+          return Meteor.users.findOne(this.author);
       }
   });
 
@@ -22,6 +25,30 @@ if (Meteor.isClient) {
       'click #navbar .new-post-button': function () {
           $('.new-post-form').toggle();
           $('.new-post-form').removeClass('hide');
+      }
+  });
+
+  Template.postAddForm.events({
+      'submit form': function (e) {
+          e.preventDefault();
+
+          var newPost = {
+              link: $('#new-post-link').val(),
+              title: $('#new-post-title').val(),
+              description: $('#new-post-description').val(),
+              author: Meteor.userId(),
+              votes: 0,
+              comments_num: 0,
+              added_on: new Date()
+          };
+
+          if (!newPost.link || !newPost.title || !newPost.description) {
+              alert("Wypełnij najpierw wszystkie pola!");
+              return;
+          }
+
+          Posts.insert(newPost);
+          $('.new-post-form').hide();
       }
   });
 
