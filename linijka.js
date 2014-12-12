49c0c20a40671ae3ade82ef7cf9c67554a33e756
Maciej <maciej@trebacz.org>
2014-12-12 18:34:22 +0100

    Dodanie obsługi kont użytkowników

--- linijka.js
+++ linijka.js
@@ -1,6 +1,10 @@
 Posts = new Meteor.Collection("posts");
 
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+  
   Template.postList.helpers({
       posts: function () {
           return Posts.find().fetch();
