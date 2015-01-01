544325d13c3a9d63a9618c5016eeec2f367c8ad5
Maciej <maciej@trebacz.org>
2015-01-01 16:22:19 +0100

    Dodawanie i wyswietlanie komentarzy

---
+++ client/views/comments.js
@@ -0,0 +1,34 @@
+Template.commentList.helpers({
+    comments: function() {
+        return Comments.find({post: this._id}, {sort: {added_on: 1}}).fetch();
+    },
+
+});
+
+Template.commentItem.helpers({
+    time: function() {
+        var added = this.added_on;
+        return moment(added).fromNow();
+    },
+    author: function() {
+        return Meteor.users.findOne(this.author);
+    }
+});
+
+Template.commentAdd.events({
+    'submit form': function (e) {
+        e.preventDefault();
+
+        var newComment = {
+            text: $('#new-comment-text').val(),
+            postId: this._id
+        };
+
+        e.target.reset();
+
+        Meteor.call('commentAdd', newComment, function(error, result) 
+        {
+            if (error) return alert(error.reason);
+        });
+    }
+});
